84e1af22cff7ba3d5e7a83c442b505102dbbebb1
Eslam Adel <eslamadel@Eslams-MacBook-Pro.local>
2021-12-16 02:36:24 +0200

    lessons 75 to 80

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,7 @@
-import Link from "next/link";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import Button from "../ui/button";
 import classes from "./event-item.module.css";
 
 function EventItem({ title, image, date, location, id }) {
@@ -16,14 +19,21 @@ function EventItem({ title, image, date, location, id }) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formatAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
