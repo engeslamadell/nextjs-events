5987b5dc38a53b165b9ab3ca6fec0613e76cbed6
Eslam Adel <eslamadel@Eslams-MacBook-Pro.local>
2021-12-16 03:13:11 +0200

    lessons 81 to 84

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,13 +5,18 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import EventAlert from "../../components/ui/error-alert";
 
 function EventDetailsPage() {
   const router = useRouter();
   const event = getEventById(router.query.eventId);
 
   if (!event) {
-    return <p>no event Found</p>;
+    return (
+      <EventAlert>
+        <p>no event Found</p>
+      </EventAlert>
+    );
   }
   return (
     <Fragment>
