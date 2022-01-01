bcb11d41a801e78cc9b2fdecd8153dc05d97fe04
Eslam Adel <eslamadel@Eslams-MacBook-Pro.local>
2022-01-01 06:08:08 +0200

    back to the events project which we implemented eariler to add features like static generation, incremental static generation, server side rendering and client side data fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,21 +1,19 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
-import { getEventById } from "../../dummy-data";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import EventAlert from "../../components/ui/error-alert";
 
-function EventDetailsPage() {
-  const router = useRouter();
-  const event = getEventById(router.query.eventId);
+function EventDetailsPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
-      <EventAlert>
-        <p>no event Found</p>
-      </EventAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
   return (
@@ -32,6 +30,27 @@ function EventDetailsPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  };
 }
 
 export default EventDetailsPage;
